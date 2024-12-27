e1209b9d07feaae11c10c2b0365a488a8ff872fe
Emil Batmanov <batmanovemil@gmail.com>
2024-12-27 06:01:28 +0300

    Добавил поддержку остальных источников литературы

--- DST.Bot/Features/GetSources/GetSources.cs
+++ DST.Bot/Features/GetSources/GetSources.cs
@@ -1,8 +1,10 @@
 ﻿using System.Text;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 using DST.Bot.Database;
 using DST.Bot.Features.Common;
 using DST.Bot.Features.StateManager;
+using HtmlAgilityPack;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
@@ -11,11 +13,16 @@ using User = DST.Bot.Entities.User;
 
 namespace DST.Bot.Features.GetSources;
 
-public static class GetSources
+public static partial class GetSources
 {
     public static IServiceCollection AddSources(this IServiceCollection services)
     {
         services.AddHttpClient("cyberleninka", o => { o.BaseAddress = new Uri("https://cyberleninka.ru/"); });
+        services.AddHttpClient("rusneb", o => { o.BaseAddress = new Uri("https://rusneb.ru/search"); });
+        services.AddHttpClient("codex", o => { o.BaseAddress = new Uri("https://docs.cntd.ru/search"); });
+        services.AddHttpClient("bigenc",
+            o => { o.BaseAddress = new Uri("https://c.bigenc.ru/content/search/suggestions"); });
+
         return services;
     }
 
@@ -62,7 +69,7 @@ public static class GetSources
         public DialogStateId DialogStateId { get; } = DialogStateId.WaitSourceQueryState;
     }
 
-    public class FetchArticlesState : IDialogState
+    public partial class FetchArticlesState : IDialogState
     {
         private readonly IHttpClientFactory _clientFactory;
         private readonly ITelegramBotClient _botClient;
@@ -80,23 +87,36 @@ public static class GetSources
 
         public async Task Handle(Message message, User user)
         {
+            string result;
             switch (message.Text)
             {
                 case "НЭБ Национальная электронная библиотека":
-                    await _botClient.SendMessage(message.Chat, "Здесь должны быть статьи из НЭБ");
+                    result = await GetRusNebArticles(user.ArticleSearchTerm);
+                    await _botClient.SendDocument(message.Chat,
+                        InputFile.FromStream(new MemoryStream(Encoding.UTF8.GetBytes(result)),
+                            $"articles-rusneb {DateTime.Now:dd-MM-yyyy hh.mm}.txt"),
+                        caption: "Найденные статьи");
                     break;
                 case "Кодекс":
-                    await _botClient.SendMessage(message.Chat, "Здесь должны быть статьи из <Кодекс>");
+                    result = await GetCodexArticles(user.ArticleSearchTerm);
+                    await _botClient.SendDocument(message.Chat,
+                        InputFile.FromStream(new MemoryStream(Encoding.UTF8.GetBytes(result)),
+                            $"articles-codex {DateTime.Now:dd-MM-yyyy hh.mm}.txt"),
+                        caption: "Найденные статьи");
                     break;
                 case "КиберЛенинка":
-                    var stream = await GetCyberLeninkaArticles(message.Text!);
+                    result = await GetCyberLeninkaArticles(user.ArticleSearchTerm);
                     await _botClient.SendDocument(message.Chat,
-                        InputFile.FromStream(stream, $"articles {DateTime.Now:dd-MM-yyyy hh.mm}.txt"),
+                        InputFile.FromStream(new MemoryStream(Encoding.UTF8.GetBytes(result)),
+                            $"articles-cyberleninka {DateTime.Now:dd-MM-yyyy hh.mm}.txt"),
                         caption: "Найденные статьи");
-                    ;
                     break;
                 case "Большая Российская Энциклопедия":
-                    await _botClient.SendMessage(message.Chat, "Здесь должны быть статьи из Большой Российской Энциклопедии");
+                    result = await GetBigEncArticles(user.ArticleSearchTerm);
+                    await _botClient.SendDocument(message.Chat,
+                        InputFile.FromStream(new MemoryStream(Encoding.UTF8.GetBytes(result)),
+                            $"articles-bigenc {DateTime.Now:dd-MM-yyyy hh.mm}.txt"),
+                        caption: "Найденные статьи");
                     break;
                 case "Отмена":
                     await _userHelper.UpdateUserState(user, DialogStateId.DefaultState);
@@ -108,17 +128,17 @@ public static class GetSources
             }
         }
 
-        private async Task<MemoryStream> GetCyberLeninkaArticles(string search)
+        private async Task<string> GetCyberLeninkaArticles(string query)
         {
             var client = _clientFactory.CreateClient("cyberleninka");
 
             var response = await client.PostAsJsonAsync("api/search",
-                new CyberLeninkaRequest { From = 0, Size = 100, Mode = "articles", Query = search });
+                new CyberLeninkaRequest { From = 0, Size = 10, Mode = "articles", Query = query });
             response.EnsureSuccessStatusCode();
 
             var responseBody = await response.Content.ReadFromJsonAsync<CyberLeninkaResponse>();
 
-            var articles = responseBody.Articles.Select(r =>
+            var articles = responseBody!.Articles.Select(r =>
                 new CyberLeninkaTelegramResponse
                 {
                     Annotation = CleanHtml(r.Annotation),
@@ -126,7 +146,7 @@ public static class GetSources
                     Link = $"https://cyberleninka.ru{r.Link}",
                     Journal = r.Journal,
                     JournalLink = $"https://cyberleninka.ru{r.JournalLink}",
-                    AuthorsString = string.Join(", ", r.Authors),
+                    AuthorsString = r.Authors == null ?  string.Empty : string.Join(", ", r.Authors),
                     Year = r.Year,
                 });
 
@@ -144,11 +164,157 @@ public static class GetSources
                 sb.AppendLine();
             }
 
-            return new MemoryStream(Encoding.UTF8.GetBytes(sb.ToString()));
+            return sb.ToString();
+        }
+
+        private async Task<string> GetCodexArticles(string query)
+        {
+            var client = _clientFactory.CreateClient("codex");
+            var sb = new StringBuilder();
+
+            try
+            {
+                var response = await client.GetStringAsync($"?q={Uri.EscapeDataString(query)}");
+                var htmlDocument = new HtmlDocument();
+                htmlDocument.LoadHtml(response);
+
+                var articleNodes = htmlDocument.DocumentNode.SelectNodes("//li[contains(@class, 'document-list_i')]");
+
+
+                if (articleNodes != null)
+                {
+                    foreach (var node in articleNodes)
+                    {
+                        var titleNode = node.SelectSingleNode(".//div[contains(@class, 'document-list_i_t')]//div");
+                        var linkNode = node.SelectSingleNode(".//a[contains(@class, 'document-list_i_lk')]");
+
+                        string title = CleanHtml(titleNode?.InnerText.Trim());
+                        string link = linkNode?.GetAttributeValue("href", string.Empty);
+
+                        if (!string.IsNullOrEmpty(link))
+                        {
+                            link = $"https://docs.cntd.ru{link}";
+                        }
+
+                        sb.AppendLine($"Название: {title}");
+                        sb.AppendLine($"Ссылка: {link}");
+                        sb.AppendLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No results found.");
+                    return "Нет результатов";
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"Request error: {e.Message}");
+                return "Возникла ошибка при запросе";
+            }
+
+            return sb.ToString();
+        }
+
+        private async Task<string> GetRusNebArticles(string query)
+        {
+            var client = _clientFactory.CreateClient("rusneb");
+            var sb = new StringBuilder();
+
+            try
+            {
+                var response = await client.GetAsync($"?q={Uri.EscapeDataString(query)}");
+                response.EnsureSuccessStatusCode();
+
+                var content = await response.Content.ReadAsStringAsync();
+
+                var htmlDoc = new HtmlDocument();
+                htmlDoc.LoadHtml(content);
+
+                var searchResults = htmlDoc.DocumentNode.SelectNodes("//div[contains(@class, 'search-list__item')]");
+                HashSet<string> uniqueTitles = [];
+
+
+                if (searchResults != null)
+                {
+                    foreach (var item in searchResults)
+                    {
+                        if (item is null) continue;
+
+                        var titleNode = item.SelectSingleNode(".//div[contains(@class, 'info_5')]/a");
+                        var title = CleanHtml(titleNode?.InnerText.Trim()!);
+
+                        if (string.IsNullOrEmpty(title)) continue;
+
+                        title = RusNebRegex().Replace(title, " ");
+                        if (!uniqueTitles.Add(title)) continue;
+
+                        var link = titleNode?.GetAttributeValue("href", string.Empty);
+                        sb.AppendLine($"Название: {title}");
+                        sb.AppendLine($"Ссылка: {link}");
+                        sb.AppendLine();
+                    }
+                }
+                else
+                {
+                    return "Нет результатов";
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"Request error: {e.Message}");
+                return "Возникла ошибка при запросе";
+            }
+
+            return sb.ToString();
+        }
+
+        private async Task<string> GetBigEncArticles(string query)
+        {
+            var client = _clientFactory.CreateClient("bigenc");
+            var sb = new StringBuilder();
+
+            try
+            {
+                var formattedQuery = query.Replace(" ", "+");
+                var response = await client.GetAsync($"?q={formattedQuery}&limit=10");
+                response.EnsureSuccessStatusCode();
+
+                var content = await response.Content.ReadAsStringAsync();
+
+                using var doc = JsonDocument.Parse(content);
+                if (doc.RootElement.TryGetProperty("list", out JsonElement list))
+                {
+                    foreach (var item in list.EnumerateArray())
+                    {
+                        string title = CleanHtml(item.GetProperty("title").GetString());
+                        string slug = item.GetProperty("link").GetProperty("slug").GetString();
+                        string fullLink = $"https://bigenc.ru/c/{slug}";
+
+                        sb.AppendLine($"Название: {title}");
+                        sb.AppendLine($"Ссылка: {fullLink}");
+                        sb.AppendLine();
+                    }
+                }
+                else
+                {
+                    return "Нет результатов";
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"Ошибка запроса: {e.Message}");
+                return "Возникла ошибка при запросе";
+            }
+
+            return sb.ToString();
         }
 
 
         public DialogStateId DialogStateId { get; } = DialogStateId.FetchArticlesState;
+
+        [System.Text.RegularExpressions.GeneratedRegex(@"\s+")]
+        private static partial System.Text.RegularExpressions.Regex RusNebRegex();
     }
 
     private static string CleanHtml(string input) => input
@@ -192,7 +358,7 @@ public static class GetSources
         public string Name { get; set; } = string.Empty;
         public string Annotation { get; set; } = string.Empty;
         public string Link { get; set; } = string.Empty;
-        public string[] Authors { get; set; } = [];
+        public string[]? Authors { get; set; } = [];
         public int Year { get; set; }
         public string Journal { get; set; } = string.Empty;
         public string JournalLink { get; set; } = string.Empty;
